1567eca5798c8c52e5094067491ea9b26eeae5ca
KINGAKWO <kingakwomakembe@gmail.com>
2025-03-30 17:18:50 +0100

    Event Planner component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+const Footer =() => {
+    return (
+        <div className="footer">
+             <footer>
+                
+                <p>&copy; Event Planner Organization. All rights reserved.</p>
+                <p>Terms of Service | Privacy Policy</p>
+
+            </footer>
+
+
+        </div>
+    );
+}
+
+export default Footer
+
